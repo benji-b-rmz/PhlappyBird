0ef973a3ea57b3d130013a66750d0592a996d1d6
benji <benji.b.ramirez@gmail.com>
2018-01-08 21:14:28 -0800

    added gameover state for playing again/exiting to menu

--- static/js/game.js
+++ static/js/game.js
@@ -11,6 +11,18 @@ var middleground;
 
 var player;
 
+var majorTextStyle = {
+    font: '40px Helvetica',
+    fill: '#ffffff',
+    fontWeight: '700'   
+};
+
+var minorTextStyle = {
+    font: '30px Helvetica',
+    fill: '#ffffff',
+    fontWeight: '700'    
+};
+
 
 /* Initialize the game */
 game = new Phaser.Game( GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, 'Phlappy Bird');
@@ -74,19 +86,11 @@ TitleScreen.prototype = {
         
         this.stage.setBackgroundColor("#0000FF");
         /* create the title, play, and instruction text*/
-        this.titleText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/3, 'PHLAPPY\n    BIRD', {
-            font: '40px Helvetica',
-            fill: '#ffffff',
-            fontWeight: '700'
-        });
+        this.titleText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/3, 'PHLAPPY\n    BIRD', majorTextStyle);
         this.titleText.anchor.set(0.5);
 
         /* play text */
-        this.playText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/2 + 40, 'PLAY', {
-            font: '15px Helvetica',
-            fill: '#ffffff',
-            fontWeight: '700'
-        });
+        this.playText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/2 + 40, 'PLAY', minorTextStyle);
         this.playText.anchor.set(0.5);
         this.playText.inputEnabled = true;
         this.playText.events.onInputDown.add(this.playGame, this);
@@ -94,11 +98,8 @@ TitleScreen.prototype = {
         /* Instructions */
         this.instructionText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/2 + 70,
             'Use SPACE or RIGHT-CLICK to fly',
-            {
-                font: '12px Helvetica',
-                fill: '#ffffff',
-                fontWeight: '500'    
-        });
+            minorTextStyle
+        );
         this.instructionText.anchor.set(0.5);
 
     },
@@ -120,6 +121,8 @@ Game.prototype = {
 
     create: function() {
         /* create the player, and input object */
+        game.stage.backgroundColor = "#0000ff";
+
         this.createPlayer();
         this.flyInput = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
@@ -128,7 +131,8 @@ Game.prototype = {
     update: function() {
 
         this.handleInput();
-    
+        
+        if (player.isOutOfBounds(GAME_HEIGHT)){ this.gameOver(), this}
     },
     
     handleInput: function() {
@@ -142,18 +146,76 @@ Game.prototype = {
     
     gameWon: function() {},
     
-    gameOver: function() {},
+    gameOver: function() {
+        console.log('gameover');
+        game.state.start('GameOver');
+
+    },
 
     createPlayer() {
 
         player = game.add.sprite( GAME_WIDTH/2, GAME_HEIGHT/2, 'player', 30);
         player.anchor.setTo(0.5);
         game.physics.arcade.enable(player);
-        player.body.collideWorldBounds = true;
+        player.body.collideWorldBounds = false;
         player.body.gravity.y = 600;
+
+        player.isOutOfBounds = function(yBound) {
+            console.log(player.body.y);
+            if (player.body.y < 0 || player.body.y > GAME_HEIGHT) {
+                console.log('OUT OF BOUNDS');
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
 
     }
 };
+
+var GameOver = function(game){};
+
+GameOver.prototype = {
+
+    create: function() {
+        /*load background, display gameover text, play again and return to title options */
+        game.stage.backgroundColor = "#ff0000";
+
+        this.gameOverText = game.add.text(GAME_WIDTH/2, GAME_HEIGHT/3, 'GAME OVER', majorTextStyle);
+        this.gameOverText.anchor.set(0.5);
+
+        this.playAgainText = game.add.text(GAME_WIDTH/2, 2*GAME_HEIGHT/3, 'PLAY AGAIN', minorTextStyle);
+        this.playAgainText.inputEnabled = true;
+        this.playAgainText.events.onInputDown.add(this.playAgain, this);
+        this.playAgainText.anchor.set(0.5);
+
+        this.exitText = game.add.text(GAME_WIDTH/2, 4*GAME_HEIGHT/5, 'EXIT TO TITLE', minorTextStyle);
+        this.exitText.inputEnabled = true;
+        this.exitText.events.onInputDown.add(this.goToMenu, this);
+        this.exitText.anchor.set(0.5);
+
+    },
+
+    update: function(){},
+
+    playAgain: function(){
+
+        game.state.start('Game');
+
+    },
+
+    goToMenu: function(){
+
+        game.state.start('TitleScreen');
+
+    }
+
+};
+
+
+
+
 
 
 /* Add the States created to the game */
@@ -161,6 +223,7 @@ game.state.add('Boot', Boot);
 game.state.add('LoadScreen', LoadScreen);
 game.state.add('TitleScreen', TitleScreen);
 game.state.add('Game', Game);
+game.state.add('GameOver', GameOver);
 
 /* start the game */
 game.state.start('Boot');
